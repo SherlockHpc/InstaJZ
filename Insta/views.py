0b5c03a6b3f27216db1ed3da11eb3ca1e68bcdbc
SherlockHpc <sherlockhpc@163.com>
2019-10-28 18:06:22 +0800

    list view and detail view

--- Insta/views.py
+++ Insta/views.py
@@ -1,6 +1,14 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from Insta.models import Post
 
 # HelloWorld is a TemplateView
 class HelloWorld(TemplateView):
     template_name = 'test.html'
     
+class PostsView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
