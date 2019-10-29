249d6428ba45b7df6cb818fcccda2e0a03898f75
SherlockHpc <sherlockhpc@163.com>
2019-10-29 17:26:23 +0800

    CRUD and auth

--- Insta/views.py
+++ Insta/views.py
@@ -1,5 +1,13 @@
 from django.views.generic import TemplateView, ListView, DetailView
 from Insta.models import Post
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
+
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from Insta.forms import CustomUserCreationForm
 
 # HelloWorld is a TemplateView
 class HelloWorld(TemplateView):
@@ -12,3 +20,24 @@ class PostsView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = '__all__'
+    login_url = 'login'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_update.html'
+    fields = ['title']
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('posts')
+
+class SignUp(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy('login')
